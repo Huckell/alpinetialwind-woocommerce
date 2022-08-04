9df623f7968d2cf7a3c6a08bda494534e1616028
Paul Huckell <42221112+Huckell@users.noreply.github.com>
2022-08-04 07:51:16 +0100

    Add Woo-Commerce Support

--- functions.php
+++ functions.php
@@ -58,4 +58,39 @@ function add_additional_class_on_li($classes, $item, $args) {
   }
   
   add_action('after_setup_theme', 'theme_images');
+
+/*woo-commerce*/
+  add_action( 'after_setup_theme', 'woocommerce_support' );
+    function woocommerce_support() {
+  add_theme_support( 'woocommerce' );
+}  
+
+/*wc image size and product grid*/
+function mytheme_add_woocommerce_support() {
+  add_theme_support( 'woocommerce', array(
+  'thumbnail_image_width' => 150,
+  'single_image_width'    => 300,
+          'product_grid'          => array(
+              'default_rows'    => 3,
+              'min_rows'        => 2,
+              'max_rows'        => 8,
+              'default_columns' => 4,
+              'min_columns'     => 2,
+              'max_columns'     => 5,
+          ),
+  ) );
+  }
+  add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
+
+  /*wc image zoom, lightbox, slider*/
+  add_action( 'after_setup_theme', 'yourtheme_setup'); 
+function yourtheme_setup() {
+    add_theme_support( 'wc-product-gallery-zoom' );
+    add_theme_support( 'wc-product-gallery-lightbox' );
+    add_theme_support( 'wc-product-gallery-slider' );
+}
+// shortcodes
+// [woocommerce_cart] – Displays your cart
+// [woocommerce_checkout] – Displays the checkout page
+// [woocommerce_my_account] – Displays the user account page
   
